8b41fb282fdaf04502bfcd93889f4833fdc6d7f3
Theodorus Jossoey <jossoey84@gmail.com>
2022-03-14 13:34:58 -0700

    Basic form setup
    Putting down a new reusable element for form and also making a wrapper components using props.children

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -5,7 +5,7 @@ const DUMMY_DATA = [
     id: "m1",
     title: "This is a first meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
@@ -14,7 +14,7 @@ const DUMMY_DATA = [
     id: "m2",
     title: "This is a second meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
